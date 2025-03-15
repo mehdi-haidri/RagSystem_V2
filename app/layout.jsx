697e3c8ea01eae917f39cef69c3c4f772e764125
mehdi haidri <mehdihaidri3@gmail.com>
2025-03-15 23:08:57 +0000

    deployment test 3

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,6 +8,10 @@ function layouts({ children }) {
   return (
               <SessionProvider>
       <html> 
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+        </head>
         <body className={"  place-items-center flex flex-row relative w-screen h-screen" }> 
 
               {children}
